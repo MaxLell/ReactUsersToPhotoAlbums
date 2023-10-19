0ad3592d7ee28496d8a7c0347255ea9573b7a06a
Max Lell <maximilian.lell@gmail.com>
2023-10-19 16:53:56 +0100

    Added a new thunk stub for removing an item from the usersList

---
+++ media/src/store/thunks/removeUser.js
@@ -0,0 +1,12 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+const removeUser = createAsyncThunk('users/remove', async (user) => {
+  const response = axios.delete(
+    `http://localhost:3005/users/${user.id}`
+  );
+  // FIX ME!!
+  return response.data;
+});
+
+export { removeUser };
