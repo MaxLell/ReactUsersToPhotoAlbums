884b654e23814b4fc204f771a69454bc9bd8ec81
Max Lell <maximilian.lell@gmail.com>
2023-10-19 17:02:56 +0100

    Extracted the function of a list Item of UsersList out into a destinctive UserListItem component

--- media/src/components/UsersList.js
+++ media/src/components/UsersList.js
@@ -4,6 +4,7 @@ import { fetchUsers, addUser } from '../store';
 import Button from './Button';
 import Skeleton from './skeleton';
 import { useThunk } from '../hooks/use-thunk';
+import UsersListItem from './UsersListItem';
 
 export default function UsersList() {
   const [doFetchUsers, isLoadingUsers, loadingUsersError] =
@@ -31,13 +32,14 @@ export default function UsersList() {
     content = <div>Oups Bugsies happened</div>;
   } else {
     content = listOfUsers.map((user) => {
-      return (
-        <div key={user.id} className="mb-2 border rounded">
-          <div className="flex p-2 justify-between items-center cursor-pointer">
-            {user.name}
-          </div>
-        </div>
-      );
+      return <UsersListItem key={user.id} user={user} />;
+      // return (
+      //   <div key={user.id} className="mb-2 border rounded">
+      //     <div className="flex p-2 justify-between items-center cursor-pointer">
+      //       {user.name}
+      //     </div>
+      //   </div>
+      // );
     });
   }
 
